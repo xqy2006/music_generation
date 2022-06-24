1074ebe4b9aaa8d636decbe94076eb236b44c92f
xqy2006 <48169260+xqy2006@users.noreply.github.com>
2022-06-25 00:40:40 +0800

    Create verify.py

---
+++ verify.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.argv[3]
+def check_contain_chinese(check_str):
+	for ch in check_str:
+		if u'\u4e00' <= ch <= u'\u9f5a':
+			pass
+		else:
+			return True
+	return False
+if check_contain_chinese(input):
+	raise Exception('error')
