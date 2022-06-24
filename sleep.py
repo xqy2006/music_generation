e209d910fe80818a9cfd0471b3ad841bbaf19846
xqy2006 <48169260+xqy2006@users.noreply.github.com>
2022-06-24 20:59:12 +0800

    Update sleep.py

--- sleep.py
+++ sleep.py
@@ -1,2 +1,15 @@
+import requests
+import os
+import urllib3
 import time
-time.sleep(90)
+urllib3.disable_warnings()
+import sys
+def download(url):
+  headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:68.0) Gecko/20100101 Firefox/68.0"
+  }
+  r = requests.get(url=url, headers=headers,verify=False)
+  return r.status_code
+while download('http://xqy2006.github.io/music_generation/'+sys.argv[1]+'.xml')==404:
+  pass
+  time.sleep(5)
