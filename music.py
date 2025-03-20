c79f79aee37d60d3f0bda6791fa5c3c8e01c4f12
xqy2006 <48169260+xqy2006@users.noreply.github.com>
2025-03-20 10:57:31 +0800

    更新 music.py

--- music.py
+++ music.py
@@ -67,7 +67,7 @@ while len(lyrics[-1]) % batch_size != 0:
     lyrics[-1].append(ord('#'))
 print(lyrics)
 lyrics = paddle.to_tensor(lyrics)
-
+print(lyrics)
 params_dict = paddle.load('Midi_Model/best_model')
 midi_model.set_dict(params_dict)
 
